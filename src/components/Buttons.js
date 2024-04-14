3fee2afe719aa081c57fb4b2ef53b069a155b41f
abraham241 <huguesmboundou@gmail.com>
2024-04-14 12:28:39 +0100

    ajout2

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -30,47 +30,28 @@ const Buttons = ({ inputHandler, clearInput, backspace, changePlusMinus, calcula
       <button className="btn clr" onClick={clearInput}>
         AC
       </button>
-      <button className="btn clr" onClick={backspace}>
-        ⌫
-      </button>
+      
       {/* <button className="btn exp" onClick={inputHandler}>
         log
       </button> */}
       <button className="btn exp" onClick={inputHandler}>
         ÷
       </button>
-      {/* <button className="btn exp" onClick={inputHandler}>
-        %
-      </button> */}
-      <button className="btn" onClick={inputHandler}>
-        7
+      <button className="btn exp" onClick={inputHandler}>
+        +
       </button>
-      <button className="btn" onClick={inputHandler}>
-        8
-      </button>
-      <button className="btn" onClick={inputHandler}>
-        9
+      <button className="btn exp" onClick={inputHandler}>
+        -
       </button>
       <button className="btn exp" onClick={inputHandler}>
         x
       </button>
       {/* <button className="btn exp" onClick={inputHandler}>
-        x<sup>3</sup>
+        %
       </button> */}
-      <button className="btn" onClick={inputHandler}>
-        4
-      </button>
-      <button className="btn" onClick={inputHandler}>
-        5
-      </button>
-      <button className="btn" onClick={inputHandler}>
-        6
-      </button>
-      <button className="btn exp" onClick={inputHandler}>
-        -
-      </button>
+      
       {/* <button className="btn exp" onClick={inputHandler}>
-        <sup>3</sup>√
+        x<sup>3</sup>
       </button> */}
       <button className="btn" onClick={inputHandler}>
         1
@@ -81,21 +62,46 @@ const Buttons = ({ inputHandler, clearInput, backspace, changePlusMinus, calcula
       <button className="btn" onClick={inputHandler}>
         3
       </button>
-      <button className="btn exp" onClick={inputHandler}>
-        +
+      <button className="btn" onClick={inputHandler}>
+        4
       </button>
+      <button className="btn" onClick={inputHandler}>
+        5
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        6
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        7
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        8
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        9
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        0
+      </button>
+      
+      {/* <button className="btn exp" onClick={inputHandler}>
+        <sup>3</sup>√
+      </button> */}
+      
+      
       {/* <button className="btn exp" onClick={inputHandler}>
         !
       </button>
       <button className="btn exp" onClick={changePlusMinus}>
         ±
       </button> */}
-      <button className="btn" onClick={inputHandler}>
-        0
-      </button>
+      
       {/* <button className="btn exp" onClick={inputHandler}>
         .
       </button> */}
+      <button className="btn clr" onClick={backspace}>
+        ⌫
+      </button>
       <button className="btn exp equal" id="equalbtn" onClick={calculateAns}>
         =
       </button>
